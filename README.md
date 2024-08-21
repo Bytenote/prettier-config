b80f0614db6a7205049aa5f49c4e41887d5cb85c
Bytenote <github@bytenote.dev>
2024-08-21 17:56:36 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# @bytenote/prettier-config
+
+Reusable Prettier configuration.
+
+## Usage
+
+**Install:**
+
+```sh
+npm i -D @bytenote/prettier-config
+```
+
+```sh
+pnpm i -D @bytenote/prettier-config
+```
+
+```sh
+bun i -D @bytenote/prettier-config
+```
+
+**Add to package.json:**
+
+```jsonc
+{
+    // ...
+    "prettier": "@bytenote/prettier-config",
+}
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ prettier.config.js
@@ -0,0 +1,35 @@
+/**
+ * @type {import('prettier').Config}
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+const config = {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'consistent',
+    jsxSingleQuote: false,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    plugins: [
+        '@ianvs/prettier-plugin-sort-imports',
+        'prettier-plugin-jsdoc',
+        'prettier-plugin-tailwindcss', // MUST come last
+    ].map(require.resolve),
+    importOrder: [
+        '^react$',
+        '<BUILTIN_MODULES>',
+        '<THIRD_PARTY_MODULES>',
+        '^[.]',
+        '^(?!.*[.]css$)[./].*$',
+        '.css$',
+    ],
+    jsdocCommentLineStrategy: 'multiline',
+    jsdocSeparateReturnsFromParam: true,
+};
+
+module.exports = config;
